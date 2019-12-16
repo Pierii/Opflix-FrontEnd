607548d9af73e463f13adf7bc120bc769a4bb5d9
Rafael Pieri Almeida <46181942866@info.local>
2019-12-16 09:41:14 -0300

    16.02.2019T09:41 - Estilização

--- Opflix/opflix-react/opflix-ui/src/pages/Lancamentos/LancamentosAdm.js
+++ Opflix/opflix-react/opflix-ui/src/pages/Lancamentos/LancamentosAdm.js
@@ -10,72 +10,59 @@ import Axios from "axios";
 
 import { Link } from "react-router-dom";
 
-class LancamentosAdm extends Component{
+class LancamentosAdm extends Component {
 
-    constructor(){
+    constructor() {
         super();
         this.state = {
             lista: []
         };
     }
 
-    componentDidMount(){
-        Axios.get('http://192.168.5.84:5000/api/lancamentos',{
+    componentDidMount() {
+        Axios.get('http://192.168.5.84:5000/api/lancamentos', {
             headers: {
-                Authorization: 'Bearer '+localStorage.getItem('usuario-opflix')
+                Authorization: 'Bearer ' + localStorage.getItem('usuario-opflix')
             }
         })
             .then(data => {
-                this.setState({lista: data.data});
+                this.setState({ lista: data.data });
                 console.log(this.state)
             })
             .catch(erro => {
                 console.log(erro);
             });
-        }
+    }
 
     render() {
-        return(
+        return (
             <div>
                 <Nav></Nav>
-                <nav className="cabecalhoPrincipal-nav">
-                            {this.state.Permissao}
-                        </nav>
                 <div className="lancamentosCorpo">
-                <Link className="login_a" to="/cadastrarLancamentos">Cadastrar Lancamentos</Link>                    
-                            <table id="tabela-lista">
-                                <thead>
-                                    <tr>
-                                        <th>Título</th>
-                                        <th>Sinopse</th>
-                                        <th>Veículo</th>
-                                        <th>Tempo de Duraação</th>
-                                        <th>IdCategoria</th>
-                                        <th>Data de lançamento</th>
-                                        <th>Formato</th>
-                                    </tr>
-                                </thead>
-                                <tbody id="tabelaListaCorpo">
-                                    {this.state.lista.map(element => {
-                                        return (
-                                            <tr>
-                                                <td>{element.titulo}</td>
-                                                <td>{element.sinopse}</td>
-                                                <td>{element.idVeiculosNavigation.veiculo}</td>
-                                                <td>{element.tempoDuracao}</td>
-                                                <td>{element.idCategoriaNavigation.categoria}</td>
-                                                <td>{element.dataLancamento}</td>
-                                                <td>{element.idFormatoNavigation.formato}</td>
-                                                <Link className="local_a" to="/localizacoes">Ver Mapa</Link>                    
-                                            </tr>
-                                        )
-                                    })}
-                                </tbody>
-                            </table>
+                    <div className="banner">
+                        <div className="buraco"></div>
+            <Link className="login_a" to="/cadastrarLancamentos">Cadastrar Lancamentos</Link>
+                    </div>
+                            {this.state.lista.map(element => {
+                                return (
+                                    <ul id="item">
+                                        <li id="element_t">{element.titulo}</li>
+                                        <div id="flex">
+                                            <li id="element">Sinopse: {element.sinopse}</li>
+                                            <li id="element">Plataforma: {element.idVeiculosNavigation.veiculo}</li>
+                                            <li id="element">Tempo: {element.tempoDuracao}</li>
+                                            <li id="element">Categoria: {element.idCategoriaNavigation.categoria}</li>
+                                            <li id="element">Release: {element.dataLancamento}</li>
+                                            <li id="element">Formato: {element.idFormatoNavigation.formato}</li>
+                                        </div>
+                                    </ul>
+                                )
+                            })}
+                            <Link className="local_a" to="/localizacoes">Ver Mapa</Link>
                 </div>
-                <Footer></Footer>
+                        <Footer></Footer>
             </div>
-        );
-    }
-}
+                    );
+                }
+            }
 export default LancamentosAdm;
